e422b53d97e79653cd857edc85a6d516193c738b
shyoon <kiekk@i-commtech.com>
2025-07-16 21:45:27 +0900

    feat: 회원 가입 통합 테스트 기능 구현

--- apps/commerce-api/src/main/kotlin/com/loopers/infrastructure/user/UserRepositoryImpl.kt
+++ apps/commerce-api/src/main/kotlin/com/loopers/infrastructure/user/UserRepositoryImpl.kt
@@ -2,11 +2,22 @@ package com.loopers.infrastructure.user
 
 import com.loopers.domain.user.UserEntity
 import com.loopers.domain.user.UserRepository
+import com.loopers.support.error.CoreException
+import com.loopers.support.error.ErrorType
 import org.springframework.stereotype.Repository
 
 @Repository
-class UserRepositoryImpl : UserRepository {
+class UserRepositoryImpl(
+    private val userMap: MutableMap<String, UserEntity> = mutableMapOf(),
+) : UserRepository {
     override fun save(userEntity: UserEntity): UserEntity {
-        TODO("Not yet implemented")
+        userMap[userEntity.userId]?.let {
+            throw CoreException(
+                errorType = ErrorType.CONFLICT,
+                customMessage = "이미 존재하는 사용자입니다: ${userEntity.userId}",
+            )
+        }
+        userMap.put(userEntity.userId, userEntity)
+        return userEntity
     }
 }
