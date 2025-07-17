c2dd808d0950bb10dc777db7ff9d423a89b7f010
shyoon <shyoon991@gmail.com>
2025-07-18 00:51:15 +0900

    refactor: 메서드명 수정
    * getPoint -> findByUserId

--- apps/commerce-api/src/main/kotlin/com/loopers/domain/point/PointRepository.kt
+++ apps/commerce-api/src/main/kotlin/com/loopers/domain/point/PointRepository.kt
@@ -3,5 +3,5 @@ package com.loopers.domain.point
 interface PointRepository {
     fun save(pointEntity: PointEntity): PointEntity
 
-    fun getPoint(userId: String): PointEntity?
+    fun findByUserId(userId: String): PointEntity?
 }
--- apps/commerce-api/src/main/kotlin/com/loopers/domain/point/PointService.kt
+++ apps/commerce-api/src/main/kotlin/com/loopers/domain/point/PointService.kt
@@ -7,12 +7,12 @@ class PointService(
     private val pointRepository: PointRepository,
 ) {
     fun chargePoint(userId: String, point: Long): PointEntity {
-        val pointEntity = pointRepository.getPoint(userId) ?: PointEntity(userId, 0L)
+        val pointEntity = pointRepository.findByUserId(userId) ?: PointEntity(userId, 0L)
         pointEntity.chargePoint(point)
         return pointRepository.save(pointEntity)
     }
 
     fun getPoint(userId: String): PointEntity? {
-        return pointRepository.getPoint(userId)
+        return pointRepository.findByUserId(userId)
     }
 }
