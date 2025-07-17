bc907e2e78b6e6f8b5ebdc9571393821d5db4d7f
shyoon <shyoon991@gmail.com>
2025-07-18 03:15:16 +0900

    feat: PointService @Transactional 추가

--- apps/commerce-api/src/main/kotlin/com/loopers/domain/point/PointService.kt
+++ apps/commerce-api/src/main/kotlin/com/loopers/domain/point/PointService.kt
@@ -1,17 +1,20 @@
 package com.loopers.domain.point
 
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class PointService(
     private val pointRepository: PointRepository,
 ) {
+    @Transactional
     fun chargePoint(userId: String, point: Long): PointEntity {
         val pointEntity = pointRepository.findByUserId(userId) ?: PointEntity(userId, 0L)
         pointEntity.chargePoint(point)
         return pointRepository.save(pointEntity)
     }
 
+    @Transactional(readOnly = true)
     fun getPoint(userId: String): PointEntity? {
         return pointRepository.findByUserId(userId)
     }
