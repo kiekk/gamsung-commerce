ce6f42dacedae7169f85a7e81919bd4ab352e498
shyoon <shyoon991@gmail.com>
2025-07-27 16:22:31 +0900

    feat: 브랜드 단위 테스트 작성
    * 브랜드명이 `한글, 영문, 숫자 20자 이내` 형식에 맞지 않으면, BrandEntity 생성에 실패한다.
    * 브랜드명, 브랜드상태가 올바른 경우 BrandEntity를 생성한다.

---
+++ apps/commerce-api/src/main/kotlin/com/loopers/domain/brand/BrandEntity.kt
@@ -0,0 +1,24 @@
+package com.loopers.domain.brand
+
+import com.loopers.domain.BaseEntity
+
+class BrandEntity(
+    val name: String,
+    val status: BrandStatusType,
+) : BaseEntity() {
+
+    enum class BrandStatusType {
+        ACTIVE,
+        INACTIVE,
+        DELETED
+    }
+
+    init {
+        !name.matches(BRAND_NAME_REGEX) && throw IllegalArgumentException("브랜드명은 한글, 영문, 숫자 20자 이내로 입력해야 합니다.")
+
+    }
+
+    companion object {
+        private val BRAND_NAME_REGEX = "^[가-힣a-zA-Z0-9]{1,20}$".toRegex()
+    }
+}
---
+++ apps/commerce-api/src/test/kotlin/com/loopers/domain/brand/BrandEntityTest.kt
@@ -0,0 +1,67 @@
+package com.loopers.domain.brand
+
+import com.loopers.domain.brand.BrandEntityFixture.Companion.aBrand
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class BrandEntityTest {
+
+    /*
+    **🧱 단위 테스트**
+    - [ ]  브랜드명이 `한글, 영문, 숫자 20자 이내` 형식에 맞지 않으면, BrandEntity 생성에 실패한다.
+    - [ ]  브랜드명, 브랜드상태가 올바른 경우 BrandEntity를 생성한다.
+    */
+    @DisplayName("브랜드를 생성할 때, ")
+    @Nested
+    inner class Create {
+        @DisplayName("브랜드명이 `한글, 영문, 숫자 20자 이내` 형식에 맞지 않으면, BrandEntity 생성에 실패한다.")
+        @ParameterizedTest
+        @ValueSource(
+            strings = [
+                "", // 빈 문자열인 경우
+                "브랜드@이름", // 특수문자가 포함된 경우
+                "브랜드 이름", // 공백이 포함된 경우
+                "브랜드이름!", // 특수문자가 포함된 경우
+                "브랜드이름1234567890abcdef", // 길이가 21인 경우
+            ],
+        )
+        fun failsToCreateBrand_whenNameIsInvalid(invalidBrandName: String) {
+            // arrange
+
+            // act
+            val result = assertThrows<IllegalArgumentException> {
+                aBrand().name(invalidBrandName).build()
+            }
+
+            // assert
+            assertAll(
+                { assertThat(result).isInstanceOf(IllegalArgumentException::class.java) },
+                { assertThat(result.message).isEqualTo("브랜드명은 한글, 영문, 숫자 20자 이내로 입력해야 합니다.") },
+            )
+        }
+
+        @DisplayName("브랜드명, 브랜드상태가 올바른 경우 BrandEntity를 생성한다.")
+        @Test
+        fun createsBrand_whenNameAndStatusAreValid() {
+            // arrange
+            val validBrandName = "브랜드이름"
+            val validBrandStatus = BrandEntity.BrandStatusType.ACTIVE
+
+            // act
+            val brandEntity = aBrand()
+                .name(validBrandName)
+                .status(validBrandStatus)
+                .build()
+
+            // assert
+            assertThat(brandEntity.name).isEqualTo(validBrandName)
+            assertThat(brandEntity.status).isEqualTo(validBrandStatus)
+        }
+    }
+}
