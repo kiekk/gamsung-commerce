296fefb90462fcbded011285ad96b2de527848d1
shyoon <shyoon991@gmail.com>
2025-07-31 19:03:40 +0900

    feat: 상품 목록 조회 쿼리 리팩토링

--- apps/commerce-api/src/main/kotlin/com/loopers/domain/product/query/ProductQueryService.kt
+++ apps/commerce-api/src/main/kotlin/com/loopers/domain/product/query/ProductQueryService.kt
@@ -3,7 +3,9 @@ package com.loopers.domain.product.query
 import com.loopers.domain.brand.QBrandEntity
 import com.loopers.domain.product.QProductEntity
 import com.loopers.domain.productlike.QProductLikeCountEntity
-import com.querydsl.core.types.dsl.BooleanExpression
+import com.loopers.support.error.CoreException
+import com.loopers.support.error.ErrorType
+import com.querydsl.core.BooleanBuilder
 import com.querydsl.jpa.impl.JPAQueryFactory
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageImpl
@@ -19,20 +21,20 @@ class ProductQueryService(
         val brand = QBrandEntity.brandEntity
         val likeCount = QProductLikeCountEntity.productLikeCountEntity
 
-        val predicates = mutableListOf<BooleanExpression?>()
-
-        condition.name?.let {
-            predicates.add(product.name.lower().like("${it.lowercase()}%"))
+        // 조건 where 절 구성
+        val predicate = BooleanBuilder().apply {
+            condition.name?.let {
+                and(product.name.lower().like("${it.lowercase()}%"))
+            }
+            condition.minPrice?.let {
+                and(product.price.value.goe(it))
+            }
+            condition.maxPrice?.let {
+                and(product.price.value.loe(it))
+            }
         }
 
-        condition.minPrice?.let {
-            predicates.add(product.price.value.goe(it))
-        }
-
-        condition.maxPrice?.let {
-            predicates.add(product.price.value.loe(it))
-        }
-
+        // 정렬 조건 구성
         val orders = pageable.sort.mapNotNull { sort ->
             when (sort.property) {
                 "name" -> if (sort.isAscending) product.name.asc() else product.name.desc()
@@ -43,6 +45,7 @@ class ProductQueryService(
             }
         }.toTypedArray()
 
+        // 목록 조회
         val productListViewModels = queryFactory
             .select(
                 QProductListViewModel(
@@ -58,18 +61,19 @@ class ProductQueryService(
             .from(product)
             .join(brand).on(product.brandId.eq(brand.id))
             .leftJoin(likeCount).on(product.id.eq(likeCount.productId))
-            .where(*predicates.filterNotNull().toTypedArray())
+            .where(predicate)
             .orderBy(*orders)
             .offset(pageable.offset)
             .limit(pageable.pageSize.toLong())
             .fetch()
 
+        // 전체 개수 조회
         val totalCount = queryFactory
             .select(product.count())
             .from(product)
             .join(brand).on(product.brandId.eq(brand.id))
             .leftJoin(likeCount).on(product.id.eq(likeCount.productId))
-            .where(*predicates.filterNotNull().toTypedArray())
+            .where(predicate)
             .fetchOne() ?: 0L
 
         return PageImpl(productListViewModels, pageable, totalCount)
