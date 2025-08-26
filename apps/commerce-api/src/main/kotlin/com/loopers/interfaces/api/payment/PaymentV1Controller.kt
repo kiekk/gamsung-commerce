cde7fb2828b9dccc086943fc422bbe1e1336f6c7
shyoon <shyoon991@gmail.com>
2025-08-27 02:40:46 +0900

    feat: 결제 스케줄러 이벤트 발행으로 변경

--- apps/commerce-api/src/main/kotlin/com/loopers/interfaces/api/payment/PaymentV1Controller.kt
+++ apps/commerce-api/src/main/kotlin/com/loopers/interfaces/api/payment/PaymentV1Controller.kt
@@ -1,6 +1,8 @@
 package com.loopers.interfaces.api.payment
 
-import com.loopers.application.order.OrderFacade
+import com.loopers.event.payload.payment.PaymentCompletedEvent
+import com.loopers.event.payload.payment.PaymentFailedEvent
+import com.loopers.event.publisher.EventPublisher
 import com.loopers.support.enums.payment.TransactionStatus
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -10,14 +12,26 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/v1/payments")
 class PaymentV1Controller(
-    private val orderFacade: OrderFacade,
+    private val eventPublisher: EventPublisher,
 ) {
 
     @PostMapping("callback")
     fun callback(@RequestBody request: PaymentV1Dto.Callback) {
         when (request.status) {
-            TransactionStatus.SUCCESS -> orderFacade.handlePaymentCompleted(request.orderId, request.transactionKey)
-            TransactionStatus.FAILED -> orderFacade.handlePaymentFailed(request.orderId, request.transactionKey)
+            TransactionStatus.SUCCESS -> eventPublisher.publish(
+                PaymentCompletedEvent(
+                    request.orderId,
+                    request.transactionKey,
+                ),
+            )
+
+            TransactionStatus.FAILED -> eventPublisher.publish(
+                PaymentFailedEvent(
+                    request.orderId,
+                    request.transactionKey,
+                ),
+            )
+
             else -> throw IllegalArgumentException("Unsupported transaction status: ${request.status}")
         }
     }
