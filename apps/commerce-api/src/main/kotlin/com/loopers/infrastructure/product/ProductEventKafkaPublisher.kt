1ec98af2c2b032a7ee11f753eb70f7d194129bff
soonho <shyoon991@gmail.com>
2025-09-05 00:28:49 +0900

    feat: 상품 조회 이벤트 발행 (#61)

--- apps/commerce-api/src/main/kotlin/com/loopers/infrastructure/product/ProductEventKafkaPublisher.kt
+++ apps/commerce-api/src/main/kotlin/com/loopers/infrastructure/product/ProductEventKafkaPublisher.kt
@@ -5,6 +5,7 @@ import com.loopers.event.Event
 import com.loopers.event.EventType
 import com.loopers.event.EventType.Topic
 import com.loopers.event.payload.product.ProductChangedEvent
+import com.loopers.event.payload.product.ProductViewedEvent
 import org.slf4j.LoggerFactory
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Component
@@ -16,6 +17,43 @@ class ProductEventKafkaPublisher(
 ) : ProductEventPublisher {
 
     private val log = LoggerFactory.getLogger(this::class.java)
+
+    override fun publish(productViewedEvent: ProductViewedEvent) {
+        log.info("[ProductEventKafkaPublisher.publish] productViewedEvent: $productViewedEvent")
+        val event = Event(
+            UUID.randomUUID().toString(),
+            EventType.PRODUCT_VIEWED,
+            productViewedEvent,
+        )
+        kafkaTemplate.send(Topic.PRODUCT_V1_VIEWED, productViewedEvent.productId.toString(), event.toJson())
+            .whenComplete { result, ex ->
+                if (ex == null) {
+                    val meta = result.recordMetadata
+                    log.info(
+                        "success to send message | topic={}, partition={}, offset={}",
+                        meta.topic(),
+                        meta.partition(),
+                        meta.offset(),
+                    )
+                } else {
+                    log.error(
+                        "fail to send message | topic= {}, partition= {}, offset= {}, ex= {}",
+                        result?.recordMetadata?.topic(),
+                        result?.recordMetadata?.partition(),
+                        result?.recordMetadata?.offset(),
+                        ex.message,
+                        ex,
+                    )
+
+                    // DLT 토픽 발행
+                    kafkaTemplate.send(
+                        Topic.PRODUCT_V1_VIEWED_DLT,
+                        productViewedEvent.productId.toString(),
+                        event.toJson(),
+                    )
+                }
+            }
+    }
 
     override fun publish(productChangedEvent: ProductChangedEvent) {
         log.info("[ProductEventKafkaPublisher.publish] productChangedEvent: $productChangedEvent")
