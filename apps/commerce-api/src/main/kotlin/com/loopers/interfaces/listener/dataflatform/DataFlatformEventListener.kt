c41125860ecefbba94b3b91ff9e08b75eb510ca6
shyoon <shyoon991@gmail.com>
2025-09-07 20:07:49 +0900

    refactor: EventListener 통합 (멘토님 피드백 반영)
    * [Q] OrderCreatedEvent 와 같은 행동 액션을 모두 handle 하고 있는데, 한 곳에서 할 수 있는 방법을 찾아주세요. 지금 코드는 적절하지는 않는 것 같아요. 행동이 추가될때마다 핸들링이 추가되어져야 하니까요"
    
    당시 코드를 살펴봤을때, 특정 이벤트에 대한 Comsumer 코드가 1:1 처럼 많았던걸로 기억해요. 그래서 각 Comsumer 가 로그를 남기고 종료하는..? 그런데, 의도한건 같은 로깅이라면 하나의 Comsumer 에서 처리될 수 있을테니 그렇게 진행되어야 하는게 맞지 않을까 싶어요.

--- apps/commerce-api/src/main/kotlin/com/loopers/interfaces/listener/dataflatform/DataFlatformEventListener.kt
+++ apps/commerce-api/src/main/kotlin/com/loopers/interfaces/listener/dataflatform/DataFlatformEventListener.kt
@@ -1,10 +1,7 @@
 package com.loopers.interfaces.listener.dataflatform
 
 import com.loopers.domain.dataflatform.DataFlatformRepository
-import com.loopers.event.payload.order.OrderCompletedEvent
-import com.loopers.event.payload.order.OrderCreatedEvent
-import com.loopers.event.payload.payment.PaymentCompletedEvent
-import com.loopers.event.payload.payment.PaymentFailedEvent
+import com.loopers.event.payload.EventPayload
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
 
@@ -12,35 +9,9 @@ import org.springframework.stereotype.Component
 class DataFlatformEventListener(
     private val dataFlatformRepository: DataFlatformRepository,
 ) {
-    @EventListener
-    fun handle(event: OrderCreatedEvent) {
-        try {
-            dataFlatformRepository.send(event)
-        } catch (e: Exception) {
-            println("데이터플랫폼 전송 실패 event: $event, error: ${e.message}")
-        }
-    }
 
     @EventListener
-    fun handle(event: PaymentCompletedEvent) {
-        try {
-            dataFlatformRepository.send(event)
-        } catch (e: Exception) {
-            println("데이터플랫폼 전송 실패 event: $event, error: ${e.message}")
-        }
-    }
-
-    @EventListener
-    fun handle(event: PaymentFailedEvent) {
-        try {
-            dataFlatformRepository.send(event)
-        } catch (e: Exception) {
-            println("데이터플랫폼 전송 실패 event: $event, error: ${e.message}")
-        }
-    }
-
-    @EventListener
-    fun handle(event: OrderCompletedEvent) {
+    fun handle(event: EventPayload) {
         try {
             dataFlatformRepository.send(event)
         } catch (e: Exception) {
