c4d23948ee72d88ae414c2de1d3f448130fe62eb
shyoon <shyoon991@gmail.com>
2025-09-11 21:28:14 +0900

    feat: 상품 랭킹 스케줄러 추가
    * carry-over용 스케줄러
    * 시뮬레이션용 스케줄러

--- apps/commerce-streamer/src/main/kotlin/com/loopers/CommerceStreamerApplication.kt
+++ apps/commerce-streamer/src/main/kotlin/com/loopers/CommerceStreamerApplication.kt
@@ -3,7 +3,9 @@ package com.loopers
 import jakarta.annotation.PostConstruct
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
+@EnableScheduling
 @SpringBootApplication
 class CommerceStreamerApplication {
 
---
+++ apps/commerce-streamer/src/main/kotlin/com/loopers/domain/productrank/ProductRankCarryOverService.kt
@@ -0,0 +1,35 @@
+package com.loopers.domain.productrank
+
+import com.loopers.support.cache.CacheRepository
+import com.loopers.support.cache.productrank.ProductRankCacheKeyGenerator
+import org.slf4j.LoggerFactory
+import org.springframework.data.redis.core.DefaultTypedTuple
+import org.springframework.stereotype.Service
+import java.time.LocalDate
+
+@Service
+class ProductRankCarryOverService(
+    private val cacheRepository: CacheRepository,
+) {
+
+    private val log = LoggerFactory.getLogger(this::class.java)
+
+    fun carryOverTomorrowRank(topN: Int, normalize: Double) {
+        log.info("[ProductRankCarryOverService.carryOverTomorrowRank] topN: {}, normalize: {}", topN, normalize)
+        val today = LocalDate.now()
+        val tomorrow = today.plusDays(1)
+
+        val productRanksWithScore = cacheRepository.findTopRankByScoreDesc(ProductRankCacheKeyGenerator.generate(today), 0, topN)
+
+        val normalizedTuples = productRanksWithScore.map { (productId, score) ->
+            DefaultTypedTuple(productId, score * normalize)
+        }.toSet()
+
+        if (normalizedTuples.isEmpty()) {
+            log.info("[ProductRankCarryOverService.carryOverTomorrowRank] 상품 랭킹이 비어있습니다. 오늘 날짜: {}", today)
+            return
+        }
+
+        cacheRepository.zAddAll(ProductRankCacheKeyGenerator.generate(tomorrow), normalizedTuples)
+    }
+}
