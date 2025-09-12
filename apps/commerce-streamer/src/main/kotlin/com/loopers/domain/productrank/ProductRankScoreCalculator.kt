e09ff67b2fa1809a53e72faede4eb770573db278
shyoon <shyoon991@gmail.com>
2025-09-12 20:06:45 +0900

    feat: 판매량 score log 함수 적용
    * 다른 집계 score 와의 편차를 줄이기 위해 사용

--- apps/commerce-streamer/src/main/kotlin/com/loopers/domain/productrank/ProductRankScoreCalculator.kt
+++ apps/commerce-streamer/src/main/kotlin/com/loopers/domain/productrank/ProductRankScoreCalculator.kt
@@ -1,5 +1,7 @@
 package com.loopers.domain.productrank
 
+import kotlin.math.log10
+
 object ProductRankScoreCalculator {
     fun calculateScoreByViewCount(viewCount: Int): Double {
         return viewCount * ProductRankScoreWeight.PRODUCT_VIEW_COUNT_WEIGHT
@@ -10,6 +12,6 @@ object ProductRankScoreCalculator {
     }
 
     fun calculateScoreBySalesCount(salesCount: Int, amount: Long): Double {
-        return (salesCount * amount) * ProductRankScoreWeight.PRODUCT_SALES_COUNT_WEIGHT
+        return log10((salesCount * amount) * ProductRankScoreWeight.PRODUCT_SALES_COUNT_WEIGHT)
     }
 }
