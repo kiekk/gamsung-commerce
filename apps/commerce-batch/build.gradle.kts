789c45a864ffaf419a318538c74a443194771471
shyoon <shyoon991@gmail.com>
2025-09-17 12:15:58 +0900

    feat: commerce-batch 모듈 추가

---
+++ apps/commerce-batch/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("org.jetbrains.kotlin.plugin.jpa")
+}
+
+dependencies {
+    // add-ons
+    implementation(project(":modules:jpa"))
+    implementation(project(":modules:redis"))
+    implementation(project(":supports:jackson"))
+    implementation(project(":supports:logging"))
+    implementation(project(":supports:monitoring"))
+
+    // web
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // batch
+    implementation("org.springframework.boot:spring-boot-starter-batch")
+
+    // batch test
+    testImplementation("org.springframework.batch:spring-batch-test")
+
+    // test-fixtures
+    testImplementation(testFixtures(project(":modules:jpa")))
+    testImplementation(testFixtures(project(":modules:redis")))
+}
